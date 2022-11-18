e4c22d4aee8ea66532551810e052e0cb8b8ad558
theit8514 <theit8514@users.noreply.github.com>
2022-11-18 17:21:58 -0500

    Add logging for enable/disable

--- src/main/java/com/theit8514/enabletransponder/EnableTransponderPlugin.java
+++ src/main/java/com/theit8514/enabletransponder/EnableTransponderPlugin.java
@@ -2,6 +2,8 @@ package com.theit8514.enabletransponder;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.fs.starfarer.api.*;
 import com.fs.starfarer.api.campaign.CampaignEventListener;
 import com.fs.starfarer.api.campaign.SectorAPI;
@@ -9,6 +11,8 @@ import com.fs.starfarer.api.campaign.listeners.ListenerManagerAPI;
 import com.theit8514.enabletransponder.listeners.*;
 
 public class EnableTransponderPlugin extends BaseModPlugin {
+    public static final Logger log = Global.getLogger(EnableTransponderPlugin.class);
+
     @Override
     public void onGameLoad(boolean newGame) {
         if (EnableTransponderConstants.DISABLED) {
@@ -19,6 +23,7 @@ public class EnableTransponderPlugin extends BaseModPlugin {
     }
 
     private void enablePlugin() {
+        log.info("Registering the EnableTranspONder plugin.");
         SectorAPI sector = Global.getSector();
         ListenerManagerAPI listenerManager = sector.getListenerManager();
         if (!listenerManager.hasListenerOfClass(LocationListener.class)) {
@@ -29,6 +34,7 @@ public class EnableTransponderPlugin extends BaseModPlugin {
     }
 
     private void disablePlugin() {
+        log.info("Unregistering the EnableTranspONder plugin. Will not handle any events.");
         SectorAPI sector = Global.getSector();
         sector.getListenerManager().removeListenerOfClass(LocationListener.class);
         List<CampaignEventListener> listeners = sector.getAllListeners();
